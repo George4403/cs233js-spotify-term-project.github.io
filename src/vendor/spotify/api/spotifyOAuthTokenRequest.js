9eb1d2ca5d58e662cb4b6b2792f3aebaa3408d67
José Bernal <jbernal.web.dev@gmail.com>
2025-12-20 13:58:04 -0800

    Implement a Request sub-class that makes a client-credentials OAuth request.

--- src/vendor/spotify/api/spotifyOAuthTokenRequest.js
+++ src/vendor/spotify/api/spotifyOAuthTokenRequest.js
@@ -1,27 +1,46 @@
-// Class to handle Spotify OAuth Token Request
-export class SpotifyOAuthTokenRequest {
-  // Initialize with client ID and client secret
-  constructor(clientID, clientSecret) {
-    this.clientID = clientID;
-    this.clientSecret = clientSecret;
+/**
+ * 
+ * Represents an HTTP Oauth Token Request to the Spotify API.
+ */
+export class SpotifyOAuthTokenRequest extends Request {
+
+
+  // The ID of the registered Spotify application that will access the API.
+  // Unnecessary to store as instance variable but included for completeness.
+  #clientId;
+
+  // The secret key associated with the Spotify application.
+  // Unnecessary to store as instance variable but included for completeness.
+  #clientSecret;
+
+  // The URL endpoint for obtaining an OAuth token from Spotify.
+  static url = "https://accounts.spotify.com/api/token";
+
+  // The HTTP method (verb) used for the request.
+  static method = "POST";
+
+  static headers = {
+    "Content-Type": "application/x-www-form-urlencoded"
+  };
+
+
+  // Construct the Request object using credentials supplied by the application.
+  constructor(clientId, clientSecret) {
+    let requestInit = {
+      method: SpotifyOAuthTokenRequest.method,
+      headers: SpotifyOAuthTokenRequest.headers,
+      body: `grant_type=client_credentials&client_id=${clientId}&client_secret=${clientSecret}`,
+    };
+    super(SpotifyOAuthTokenRequest.url, requestInit);
   }
 
-  // Fetch the access token from Spotify API
-  async getAccessToken() {
-    const authParameters = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-      body: `grant_type=client_credentials&client_id=${this.clientID}&client_secret=${this.clientSecret}`,
-    };
-    // Make the request to get the access token
-    const response = await fetch(
-      "https://accounts.spotify.com/api/token",
-      authParameters
-    );
-    // Parse the JSON response to extract the access token
-    const data = await response.json();
-    return data.access_token;
+  // NOTE: This pattern won't work and won't override the URL provided in the constructor/super() call.
+  // See above for a working implementation.
+  get url() {
+    return "https://foobar";//SpotifyOAuthTokenRequest.url;
   }
+
+
+
+
 }
