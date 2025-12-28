7ec8e6461e598cdae6970b1e107f280c298817c9
José Bernal <jbernal.web.dev@gmail.com>
2025-12-28 15:29:59 -0800

    Rename entries; import global identifiers for react.

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,22 @@ const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
+/*
+  _jsx(): This function is used by the compiler for JSX elements that have dynamic children (e.g., <MyComponent>{someVariable}</MyComponent>).
+
+  _jsxs(): The "s" stands for "static" or "spanning". This is an optimization used for elements with multiple, static children.  For example:
+  
+    <div>
+      <span>Static 1</span>
+      <span>Static 2</span>
+    </div>
+    
+    It handles children more efficiently by passing them as a single, combined array, which allows for better performance by skipping certain runtime checks and manipulations.
+
+  Performance: The primary benefit of the jsxs() function is a performance improvement. By optimizing the handling of static children, it can potentially result in smaller bundle sizes and slightly faster rendering, as it avoids array slicing and other runtime overheads associated with the older React.createElement or jsx() methods for these specific cases. 
+*/
+
+
 module.exports = (env) => {
 
   console.log(env);
@@ -9,8 +25,8 @@ module.exports = (env) => {
   return {
     mode: 'development',
     entry: {
-      spotify: './src/js/App.js',
-      jsx: './src/js/MyComponent.js'
+      spotify: './src/js/index.js',
+      jsx: './src/js/SpotifyApp.js'
     },
     output: {
       path: path.resolve(__dirname, "dist"),
@@ -51,9 +67,11 @@ module.exports = (env) => {
       ],
     },
     plugins: [
-      // new Dotenv(),
       new webpack.ProvidePlugin({
-        jsx: ["@ocdla/view/jsx-runtime", "default"]
+        jsx: ["react/jsx-runtime", "default"],
+        jsxs: ["react/jsx-runtime", "jsxs"], // 
+        Fragment: ["react/jsx-runtime", "Fragment"],
+
       }),
       new HtmlWebpackPlugin({
         template: path.resolve(__dirname, "./src/index.html"),
