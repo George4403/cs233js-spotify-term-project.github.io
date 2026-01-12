8eee22f5ef3057c03bd9de857d4819fdc9d4469a
José Bernal <jbernal.web.dev@gmail.com>
2026-01-12 00:07:53 -0800

    Format rankings.

--- src/js/components/ChartEntry.js
+++ src/js/components/ChartEntry.js
@@ -2,17 +2,16 @@ import Album from "./Album.js";
 import { Track } from "./Track.js";
 
 export default function ChartEntry({ track, rank }) {
-    // create a new li element to hold the chart entry
+    // Create a new li element to hold the chart entry.
     const li = document.createElement("li");
+    const rankElem = document.createElement("span");
+    rankElem.classList.add("chart-rank");
+    rankElem.appendChild(document.createTextNode(`#${rank}`));
     li.classList.add("track-list-item");
-    li.appendChild(document.createTextNode(`# ${rank}`));
+
+    li.appendChild(rankElem);
 
     li.appendChild(Track({ track }));
-    /*
-    // create a fragment to hold the album and track components
-    let fragment = document.createDocumentFragment();
-    fragment.appendChild(Album({}));
-    fragment.appendChild(Track({ track }));*/
 
     return li;
 }
